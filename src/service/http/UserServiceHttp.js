438d9ae79cc95fb3a9a8d78febf52977cc00031e
malves224 <malves224@gmail.com>
2022-06-27 17:49:18 -0300

    refactor remove try catch desnecessarios.

--- src/service/http/UserServiceHttp.js
+++ src/service/http/UserServiceHttp.js
@@ -15,52 +15,31 @@ class UserServiceHttp {
     }
 
     async authenticate(loginInfo, action = 'login') {
-      try {
-        const response = await this.instanceAxios
-          .post(`${this.model}/${action}`, loginInfo);
-        this.instanceAxios.defaults.headers = { authorization: response.data.token };
-        this.localStorage.get();
-        console.log(response);
-      } catch (error) {
-        console.log(error.response);
-      }
+      const response = await this.instanceAxios
+        .post(`${this.model}/${action}`, loginInfo);
+      this.instanceAxios.defaults.headers = { authorization: response.data.token };
+      return response;
     }
 
     async create(payload) {
-      try {
-        const response = await this.instanceAxios(this.model, payload);
-        return response;
-      } catch (error) {
-        console.log(error);
-      }
+      const response = await this.instanceAxios(this.model, payload);
+      return response;
     }
 
     async findAll() {
-      try {
-        const users = await this.instanceAxios(this.model);
-        return users;
-      } catch (error) {
-        console.log(error.response);
-      }
+      const users = await this.instanceAxios(this.model);
+      return users;
     }
 
     async findOne(id) {
-      try {
-        const user = await this.instanceAxios(this.model, { params: { id } });
-        return user;
-      } catch (error) {
-        console.log(error.response);
-      }
+      const user = await this.instanceAxios(this.model, { params: { id } });
+      return user;
     }
 
     async buyProduct({ idProduct, quantity }) {
-      try {
-        const response = await this.instanceAxios
-          .post(`${this.model}/sale`, { idProduct, quantity });
-        return response;
-      } catch (error) {
-        console.log(error.response);
-      }
+      const response = await this.instanceAxios
+        .post(`${this.model}/sale`, { idProduct, quantity });
+      return response;
     }
 
     async update(idUser) {
