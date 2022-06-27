9e3f27e96fe9b0be207c02a9c5e7280110923a9a
malves224 <malves224@gmail.com>
2022-06-27 16:45:54 -0300

    feat(HeaderNav): add estrutura html inicial

--- src/components/HeaderNav/HeaderNav.vue
+++ src/components/HeaderNav/HeaderNav.vue
@@ -1,17 +1,63 @@
 <template>
-    <div>
-        <header>
-            <p>logo</p>
-            <p>info usuario</p> <!--condicional apenas se o usuario tiver logado-->
-            <button>Sair ou Voltar</button>
-        </header>
-    </div>
+    <header>
+        <div class="header-nav">
+            <div class="header-action">
+                <h1>ALVESHOP</h1>
+                <div>
+                    <!--condicional apenas se o usuario tiver logado-->
+                    <p>Rogeirinho</p>
+                    <p class="coins">Coins: $ 150,30</p>
+                </div>
+                <!--if userLogado else-->
+                <b-button v-if="false" variant="secondary">
+                    Sair
+                </b-button>
+                <router-link v-else :to="inLogin ? '/' : '/Login'">
+                    <b-button variant="danger">
+                        {{ inLogin ? 'Voltar' : 'Login' }}
+                    </b-button>
+                </router-link>
+            </div>
+            <SearchBar></SearchBar>
+        </div>
+    </header>
 </template>
 <script>
-export default {
+import SearchBar from './SearchBar.vue';
 
+export default {
+  name: 'HeaderNav',
+  components: { SearchBar },
+  computed: {
+    inLogin() {
+      return this.$route.name === 'login';
+    },
+  },
 };
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+.header-nav {
+    align-items: center;
+    flex-direction: column;
+    display: flex;
+    height: 150px;
+    padding: 10px 0;
+    justify-content: space-between;
+}
 
+.header-action {
+    align-items: center;
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+}
+
+.coins {
+    color: gold;
+}
+
+button {
+    width: 66px;
+    max-width: 66px;
+}
 </style>
---
+++ src/components/HeaderNav/SearchBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="search-bar">
+        <b-input-group class="mb-2">
+            <b-form-input type="search" placeholder="Pesquse o que você quer..." />
+            <b-input-group-prepend is-text>
+                <b-icon icon="search" />
+            </b-input-group-prepend>
+        </b-input-group>
+    </div>
+</template>
+<script>
+export default {
+  name: 'SearchNav',
+};
+</script>
+<style lang="scss">
+</style>
