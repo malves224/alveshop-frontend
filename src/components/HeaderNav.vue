41ea8d21bea3e4dac51be93a844f3d90032e521f
malves224 <malves224@gmail.com>
2022-06-27 15:31:56 -0300

    style estrutura inicial de componentes: HeaderNav, HomeView, e LoginView.

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header>
+            <p>logo</p>
+            <p>info usuario</p> <!--condicional apenas se o usuario tiver logado-->
+            <button>Sair ou Voltar</button>
+        </header>
+    </div>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style lang="scss">
+
+</style>
