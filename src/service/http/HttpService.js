b96add38361ea8cfa43755dc0b8d7465465bdb76
malves224 <malves224@gmail.com>
2022-06-28 17:03:23 -0300

    feat(HttpService) add actions para abrir alerta na interface.

--- src/service/http/HttpService.js
+++ src/service/http/HttpService.js
@@ -1,11 +1,13 @@
 import router from '@/router';
 import axios from 'axios';
+import { mapActions } from 'vuex';
 import LocalStorage from '../localStorage/LocalStorage';
 
 class HttpService {
   constructor() {
     this.baseURL = process.env.VUE_APP_API_BASE_URL || 'http://localhost:3001';
     axios.defaults.baseURL = this.baseURL;
+    this.actionsVuex = { ...mapActions(['openAlert']) };
 
     axios.interceptors.request.use((config) => {
       const userInfo = LocalStorage.get('credentials');
@@ -19,12 +21,11 @@ class HttpService {
       const { data: { message } } = response;
       const isAuthenticated = !(message.includes('não encontrado')
         || message.includes('invalido'));
+      this.actionsVuex.openAlert({ message, variant: 'danger' });
       if (!isAuthenticated) {
         router.push({ name: 'login' });
-        console.log('redirect login com mensagem', message);
         return;
       }
-      console.log('settar mensagem: ', message);
       return error;
     });
   }
