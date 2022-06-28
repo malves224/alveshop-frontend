17db22858e980131d48676e2056d16ece1cdbb29
malves224 <malves224@gmail.com>
2022-06-28 17:03:42 -0300

    feat(app) implementa componente Alert.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="app">
-    <HeaderNav></HeaderNav>
-    <router-view></router-view>
+      <AlertVue
+        :message="getAllert.message"
+        :variant="getAllert.variant"
+        :open="getAllert.isOpen"
+      />
+      <HeaderNav></HeaderNav>
+      <router-view></router-view>
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
 import HeaderNav from './components/HeaderNav/HeaderNav.vue';
+import AlertVue from './components/widgets/alert/AlertVue.vue';
 
 export default {
-  components: { HeaderNav },
+  components: { HeaderNav, AlertVue },
+  computed: {
+    ...mapGetters(['getAllert']),
+  },
 };
 </script>
 <style>
