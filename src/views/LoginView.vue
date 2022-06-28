9360d562b6c132a59fa6133d26c3acf516a39a99
malves224 <malves224@gmail.com>
2022-06-28 14:48:02 -0300

    style(LoginView) tamanho e posicionamento do card.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,12 +1,12 @@
 <template>
-    <b-card>
-        <component :is="ActionComponent" />
-        <b-button
-          @click="changeAction"
-        >
-          {{inLogin ? "CADASTRAR" : "VOLTAR"}}
-        </b-button>
+  <div class="container-login">
+    <b-card class="card-login">
+      <component :is="ActionComponent" />
+      <b-button @click="changeAction">
+        {{ inLogin ? "CADASTRAR" : "VOLTAR" }}
+      </b-button>
     </b-card>
+  </div>
 </template>
 <script>
 import RegisterCard from '@/components/Login/RegisterCard.vue';
@@ -33,5 +33,18 @@ export default {
 };
 </script>
 <style lang="scss">
+.container-login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 30px 0 0;
+}
 
+.card-login {
+  display: flex;
+  width: 40%;
+  min-width: 250px;
+  max-width: 320px;
+  height: 280px;
+}
 </style>
