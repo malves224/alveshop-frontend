33648e7b16a9811a2265cc716d46ac2f8e3e22c2
malves224 <malves224@gmail.com>
2022-06-28 10:46:39 -0300

    refactor(HttpService) classe que configura os interceptadores do axio.

---
+++ src/service/http/HttpService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import LocalStorage from '../localStorage/LocalStorage';
+
+class HttpService {
+  constructor() {
+    this.baseURL = process.env.VUE_APP_API_BASE_URL || 'http://localhost:3001';
+    axios.defaults.baseURL = this.baseURL;
+
+    axios.interceptors.request.use((config) => {
+      const userInfo = LocalStorage.get('credentials');
+      const token = userInfo ? userInfo.token : '';
+      config.headers = { Authorization: token };
+      return config;
+    });
+
+    axios.interceptors.response.use((response) => response, (error) => {
+      const { response } = error;
+      const { data: { message } } = response;
+      const isAuthenticated = !(message.includes('não encontrado')
+        || message.includes('invalido'));
+      if (!isAuthenticated) {
+        console.log('redirect login com mensagem');
+        return;
+      }
+      console.log('settar mensagem: ', message);
+      return error;
+    });
+  }
+}
+
+export default HttpService;
