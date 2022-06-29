a7ed2780e3dc5b99c1e49dc20e99f6c03709b427
malves224 <malves224@gmail.com>
2022-06-29 10:51:59 -0300

    feat (main) importa plugin request.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import '@/plugins/httpRequests';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
