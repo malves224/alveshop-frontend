fd39dc1ef661713fc807cae938da0b1482b44c46
malves224 <malves224@gmail.com>
2022-06-29 13:10:47 -0300

    feat(LoginCard.vue) autenticação do usuario.

--- src/components/Login/LoginCard.vue
+++ src/components/Login/LoginCard.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="container-form">
     <h2>Login</h2>
-    <b-input placeholder="Digite seu email" id="email" />
-    <b-input placeholder="Digite sua senha" type="password" />
-    <b-button variant="danger">Acessar conta</b-button>
+    <b-input v-model="email" placeholder="Digite seu email" id="email" />
+    <b-input
+      v-model="password"
+      placeholder="Digite sua senha"
+      type="password"
+    />
+    <b-button @click="login" variant="danger"
+      >{{ loading ? "Aguarde.." : "Acessar conta" }}
+    </b-button>
   </div>
 </template>
 <script>
 export default {
   name: 'LoginCard',
+  data() {
+    return {
+      email: '',
+      password: '',
+      loading: false,
+    };
+  },
+  methods: {
+    login() {
+      this.loading = true;
+      this.$userService
+        .authenticate({
+          email: this.email,
+          password: this.password,
+        })
+        .then(() => this.$router.push('/'))
+        .finally(() => { this.loading = false; });
+    },
+  },
 };
 </script>
 <style lang="scss">
