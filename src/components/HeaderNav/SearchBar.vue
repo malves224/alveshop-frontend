af21ef6610691827680316993b2af2bf78f3c7b2
malves224 <malves224@gmail.com>
2022-06-29 16:27:52 -0300

    style(components/SearchBar)

--- src/components/HeaderNav/SearchBar.vue
+++ src/components/HeaderNav/SearchBar.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="search-bar">
-        <b-input-group class="mb-2">
-            <b-form-input type="search" placeholder="Pesquse o que você quer..." />
-            <b-input-group-prepend is-text>
-                <b-icon icon="search" />
-            </b-input-group-prepend>
-        </b-input-group>
+        <b-input placeholder="Pesquse o que você quer...">
+        </b-input>
+        <p class="d-flex align-items-center mb-0 pr-2 pl-1">
+            <b-icon icon="search" />
+        </p>
     </div>
 </template>
 <script>
@@ -13,5 +12,22 @@ export default {
   name: 'SearchNav',
 };
 </script>
-<style lang="scss">
+<style scoped lang="scss">
+    .search-bar {
+        display: flex;
+        align-items: center;
+        background-color: #202020;
+        width: 40%;
+        height: 45px;
+        max-width: 420px;
+        border-radius: 12px;
+        box-shadow: 3px 3px 10px 4px rgb(40 40 40 / 89%);
+
+    }
+    input, input:focus {
+        background-color: transparent;
+        border: none;
+        color: white;
+        box-shadow: none;
+    }
 </style>
