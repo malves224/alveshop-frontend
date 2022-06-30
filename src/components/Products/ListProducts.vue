a7718cf5f6322a4d7b4bac3125414f2efebd63ed
malves224 <malvse224@gmail.com>
2022-06-30 11:34:21 -0300

    feat(ListProducts) renderização dos cards dos produtos, faltando aplicar style.

---
+++ src/components/Products/ListProducts.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="list-products">
+    <div class="card-product" v-for="product in getProducts" :key="product.id">
+      <img
+        :width="100"
+        :src="product.urlImage"
+        :alt="`imagem do produto ${product.name}`"
+      />
+      <div class="text-product">
+        <p>{{ product.name }}</p>
+        <p>
+          <span>Preço: </span>
+          {{ product.price }}
+        </p>
+      </div>
+      <b-button :variant="product.active ? 'danger' : ''">
+        {{ product.active ? "COMRAR" : "INDISPONIVEL" }}
+      </b-button>
+    </div>
+  </div>
+</template>
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'HomeView',
+  computed: {
+    ...mapGetters(['getProducts']),
+  },
+  methods: {
+    ...mapActions(['loadingProducts']),
+  },
+  mounted() {
+    this.loadingProducts();
+  },
+};
+</script>
+<style lang="scss">
+.list-products {
+    display: flex;
+    flex-flow: row wrap;
+}
+</style>
